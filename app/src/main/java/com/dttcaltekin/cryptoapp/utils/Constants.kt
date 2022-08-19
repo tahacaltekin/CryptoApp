854607a4b53e994cad800b8d1b0085e7413193d3
tahacaltekin <thcaltekin@gmail.com>
2022-08-19 08:25:51 +0300

    Utils package

---
+++ app/src/main/java/com/dttcaltekin/cryptoapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.dttcaltekin.cryptoapp.utils
+
+object Constants {
+    const val BASE_URL = "https://pro-api.coinmarketcap.com/"
+    const val API_KEY = "59ec4dae-221e-4d2b-81d6-985c8a7407de"
+    const val LIMIT = "100"
+}
