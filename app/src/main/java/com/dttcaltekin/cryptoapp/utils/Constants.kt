4d23e7f189e36407bd5231be738b185d5dbc5311
tahacaltekin <thcaltekin@gmail.com>
2022-08-19 08:52:43 +0300

    Api Key deleted

--- app/src/main/java/com/dttcaltekin/cryptoapp/utils/Constants.kt
+++ app/src/main/java/com/dttcaltekin/cryptoapp/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.dttcaltekin.cryptoapp.utils
 
 object Constants {
     const val BASE_URL = "https://pro-api.coinmarketcap.com/"
-    const val API_KEY = "59ec4dae-221e-4d2b-81d6-985c8a7407de"
+    const val API_KEY = "YOUR_API_KEY_HERE"
     const val LIMIT = "100"
 }
